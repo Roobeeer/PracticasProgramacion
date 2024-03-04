0362eec73d3baa81fdfeaa3329f55477a8955dfa
Roobeeer <furybtw1@gmail.com>
2024-03-04 20:53:11 +0100

    main

--- TiendaP1/src/shop/ClothingItem.java
+++ TiendaP1/src/shop/ClothingItem.java
@@ -1,4 +1,71 @@
 package shop;
 
 public class ClothingItem {
+    private String name;
+    private double price;
+    private char size;
+
+    public ClothingItem(String name, double price, char size){
+        this.name = name;
+        this.price = price;
+    if(size == 'S' || size == 'M' || size == 'L' ){
+        this.size = size;
+    }else{
+        System.out.println("Tamaño no válido. Se asignará S por defecto");
+        this.size = 'S';
+    }
+    }
+
+
+    public String dameName(){
+        return name;
+    }
+    public void ponName(String name){
+        this.name = name;
+    }
+
+    public double damePrice(){
+        return price;
+    }
+
+    public void ponPrice(double price){
+        this.price = price;
+    }
+
+    public char dameSize(){
+        return size;
+    }
+    public void ponSize(char size){
+        if(size == 'S'|| size == 'M' || size == 'L') {
+            this.size = size;
+        }else{
+            System.out.println("Tamaño inválido, no se ha modificado");
+        }
+    }
+
+    @Override
+    public String toString(){
+        return "ClothingItem = name: "+name+"  ||  price: "+price+"  ||  size: "+size;
+    }
+}
+
+class TestClothingItem{
+    public static void checkClass(Class<?> clazz) {
+        if(clazz.getName().equals("shop.ClothingItem")){
+            System.out.println("¡La clase es correcta!");
+        }else{
+            System.out.println("La clase no es correcta");
+        }
+    }
+
+
+    public static void main(String[] args) {
+        ClothingItem item1 = new ClothingItem("Camisa",25.99,'M');
+
+        if(item1 != null){
+            checkClass(item1.getClass());
+            System.out.println(item1);
+        }
+
+    }
 }
--- TiendaP1/src/shop/Inventory.java
+++ TiendaP1/src/shop/Inventory.java
@@ -1,4 +1,87 @@
 package shop;
 
 public class Inventory {
-}
+
+    private static final int MAX_SIZE = 10;
+    private ClothingItem[] items;
+    private int itemLength;
+
+    public Inventory(){
+        this.items = new ClothingItem[MAX_SIZE];
+        this.itemLength = 0;
+
+    }
+
+    public int dameItemCount(){
+        return itemLength;
+    }
+
+    public void añadirItem(ClothingItem newItem){
+        if(itemLength<MAX_SIZE){
+            items[itemLength] = newItem;
+            itemLength++;
+        }else{
+            System.out.println("Demasiadas cosas en el inventario, elimina algo");
+        }
+    }
+
+
+    public int checkStock(String name, char size){
+        int count = 0;
+        for(int i= 0;i<itemLength;i++){
+            ClothingItem currentItem = items[i];
+            if(currentItem != null && currentItem.dameName().equals(name) && currentItem.dameSize()==size){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public void eliminarItem(String name, char size){
+        int stockContador = checkStock(name,size);
+        if(stockContador>0){
+            for(int i = 0;i<itemLength;i++){
+                ClothingItem currentItem = items[i];
+                if(currentItem !=null && currentItem.dameName().equals(name) && currentItem.dameSize()==size){
+                    for(int j = i;j<itemLength-1;j++){
+                        items[j] = items[j+1];
+                    }
+                    items[itemLength-1] = null;
+                    itemLength--;
+                    break;
+                }
+            }
+        }else{
+            System.out.println("No hay stock");
+        }
+
+    }
+    public ClothingItem extractItem(String name, char size) {
+        int stockCount = checkStock(name, size);
+        if (stockCount > 0) {
+            for (int i = 0; i < itemLength; i++) {
+                ClothingItem currentItem = items[i];
+                if (currentItem != null && currentItem.dameName().equals(name) && currentItem.dameSize() == size) {
+                    eliminarItem(name,size);  // Eliminar la prenda del stock
+                    return currentItem;
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder("Inventario:\n");
+        result.append("Cantidad de prendas en stock: ").append(itemLength).append("\n");
+        result.append("Capacidad máxima del inventario: ").append(MAX_SIZE).append("\n");
+        result.append("Items en el inventario:\n");
+
+        for (int i = 0; i < itemLength; i++) {
+            result.append("\t").append(items[i]).append("\n");
+        }
+
+        return result.toString();
+    }
+
+    }

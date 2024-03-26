beb75bbe29b607c7a2482a73d84f2a4220eaeeff
Roobeeer <furybtw1@gmail.com>
2024-03-26 09:49:10 +0100

    main

--- TiendaP1EnClase/src/sales1/SalesRegister1.java
+++ TiendaP1EnClase/src/sales1/SalesRegister1.java
@@ -1,4 +1,36 @@
 package sales1;
+import shop1.*;
 
 public class SalesRegister1 {
+    private long totalSalesCount;
+    private double totalSalesAmount;
+
+
+    public String processSale(Inventory1 inventory, String name, char size){
+       int hayStock = inventory.checkStock(name,size);
+       if(hayStock !=0){
+           inventory.extractItem(name,size);
+           totalSalesCount++;
+           totalSalesAmount++;
+           return name;
+
+       }else{
+           System.out.println("El producto no está en stock");
+           System.exit(0);
+            return null;
+       }
+    }
+
+    public long getTotalSalesCount(){
+        return totalSalesCount;
+    }
+
+    public double getTotalSalesAmount(){
+        return totalSalesAmount;
+    }
+
+    public long getBalance(){
+
+
+    }
 }
